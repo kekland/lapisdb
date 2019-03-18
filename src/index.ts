9253cb0312f595ff384e96963a5aeddab91018b8
Erzhan <kk.erzhan@gmail.com>
2019-03-18 11:35:23 +0600

    Preparing for NPM.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,24 @@
-import { Human } from './test/human.model';
-import { Factory } from './factory';
-import { IFilter } from './database/filter/filter.types';
-import { Datastore } from './database/datastore/datastore';
-import { SortOperator } from './database/sort/sort';
-import { SortDirection } from './database/sort/sort.types';
-import * as moment from 'moment'
-import { PushOperation } from './database/operations/push.operation';
-import { EditOperation } from './database/operations/edit.operation';
+import { Datastore } from "./database/datastore/datastore";
+import { BatchedPushOperation, PushOperation } from "./database/operations/push.operation";
+import { GetOperation } from "./database/operations/get.operation";
+import { EditOperation } from "./database/operations/edit.operation";
+import { DeleteOperation, BatchedDeleteOperation } from "./database/operations/delete.operation";
+import { Model } from "./database/model/model";
+import { FilterOperator } from "./database/filter/filter";
+import { SortOperator } from "./database/sort/sort";
+import { DatastoreOperations } from "./database/datastore/datastore.methods";
+import { IEdit } from "./database/types/edit.types";
 
-const bootstrap = async () => {
-  const store = new Datastore<Human>('hello', './database', () => Human)
-
-  /*const items = await store.pushBatched()
-    .item(store.create(new Human('lmao2', 17, ['xd 1', 'xd 2'])))
-    .item(store.create(new Human('lmao1', 1, [])))
-    .item(store.create(new Human('lmao 3', 891, [], 'lmao 111')))
-    .push()*/
-
-  //console.log(items)
-
-  const item = await store.get().where((value) => value.meta.updated > value.meta.created).
-  console.log(item)
-  //const item = await store.push().item(new Human('xd', 18, [])).run()
- //console.log(item)
+export default {
+  Datastore,
+  DatastoreOperations,
+  GetOperation,
+  PushOperation,
+  BatchedPushOperation,
+  EditOperation,
+  DeleteOperation,
+  BatchedDeleteOperation,
+  Model,
+  FilterOperator,
+  SortOperator
 }
-
-bootstrap()
