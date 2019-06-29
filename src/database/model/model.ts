974aa4e567a6cf1c4d9501c0964be54f9e8bdf1c
kekland <kk.erzhan@gmail.com>
2019-06-29 13:26:19 +0600

    added methods for Model

--- src/database/model/model.ts
+++ src/database/model/model.ts
@@ -7,14 +7,30 @@ export class Model<T extends Model<T>> {
   public meta: IModelMetadata;
 
   @Exclude({ toPlainOnly: true })
-  private store?: () => Datastore<T>;
+  private store: () => Datastore<any>;
 
   constructor(store: Datastore<T>) {
     this.store = () => store;
     this.meta = {} as IEmptyModelMetadata;
   }
 
-  hasMetadata(): this is {meta: IFullfilledModelMetadata} {
+  hasMetadata(): this is { meta: IFullfilledModelMetadata } {
     return this.meta.id != null && this.meta.created != null && this.meta.updated != null;
+  }
+
+  async save() {
+    if (!this.hasMetadata()) {
+      throw Error('Item has no metadata.')
+    }
+
+    await this.store().adapter.put(this)
+  }
+
+  async delete() {
+    if (!this.hasMetadata()) {
+      throw Error('Item has no metadata.')
+    }
+
+    await this.store().adapter.remove(this.meta.id)
   }
 }
