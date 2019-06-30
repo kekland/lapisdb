86a6e8a55322b54b4fa99472e00da02668294f3c
kekland <kk.erzhan@gmail.com>
2019-06-30 12:58:50 +0600

    forgot to export new stuff

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,16 @@
 import 'reflect-metadata'
 
-import { DatastoreAdapter } from './lapisdb/datastore/adapters/adapter'
-
-import { ClassType } from './lapisdb/datastore/interfaces/class.type'
-import { FilterMethod } from './lapisdb/datastore/interfaces/filter.type'
-import { IPaginationData } from './lapisdb/datastore/interfaces/pagination.type'
-import { LevelIterator, ModelIterable } from './lapisdb/datastore/interfaces/model.iterator'
-
-import { Datastore } from './lapisdb/datastore/datastore'
-
-import { Model } from './lapisdb/model/model'
-import { IEmptyModelMetadata, IFilledModelMetadata, IModelMetadata } from './lapisdb/model/model.metadata'
-
-import { GetOperation } from './lapisdb/operations/get.operation'
-import { PushOperation, BatchedPushOperation } from './lapisdb/operations/push.operation'
-import { DeleteOperation, BatchedDeleteOperation } from './lapisdb/operations/delete.operation'
-
-export {
-  DatastoreAdapter,
-  ClassType,
-  FilterMethod,
-  IPaginationData,
-  LevelIterator,
-  ModelIterable,
-  Datastore,
-  Model,
-  IEmptyModelMetadata,
-  IFilledModelMetadata,
-  IModelMetadata,
-  GetOperation,
-  PushOperation,
-  BatchedPushOperation,
-  DeleteOperation,
-  BatchedDeleteOperation,
-}
+export * from './lapisdb/datastore/adapters/adapter'
+export * from './lapisdb/datastore/datastore'
+export * from './lapisdb/datastore/datastore.manager'
+export * from './lapisdb/datastore/interfaces/class.type'
+export * from './lapisdb/datastore/interfaces/filter.type'
+export * from './lapisdb/datastore/interfaces/model.iterator'
+export * from './lapisdb/datastore/interfaces/pagination.type'
+export * from './lapisdb/datastore/metadata.utils'
+export * from './lapisdb/model/model'
+export * from './lapisdb/model/model.metadata'
+export * from './lapisdb/model/reference'
+export * from './lapisdb/operations/get.operation'
+export * from './lapisdb/operations/push.operation'
+export * from './lapisdb/operations/delete.operation'
